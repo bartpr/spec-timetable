14498d51129e2c9f8439f9a4aca666be14590c27
PErSJa <testet@interia.eu>
2013-05-23 16:55:43 +0200

    nie dziala tylko dla pusha

--- data.cpp
+++ data.cpp
@@ -1,12 +1,31 @@
 #include "data.h"
-unsigned short Data::knot::knots = 0;
+
+//id i poziomy liczone s¹ od 1
+//pozniej wejda jako static
+short cl= 1;//Current_level poziom drzewa na ktorym jestesmy
+short ci= 1;//current_id obecne id
+unsigned short knot::knots = 0;
+
+short Dolans_t[3]={ 2, 3, 4 };
 
 Data::Data(unsigned char numberOfLessons)
 {
     tab = new Lesson[numberOfLessons];
 }
 
-void Data::Make_Forest( short *Dolans_t, knot *k ){
+void Data::Make_Forest( short *D_t ){
+	this->k= new knot;
+	int m=0;
+	while( D_t != '\0' ){
+		m++;
+	}
+	for( int i= 0; i< m; i++ ){
+	
+	}
+	
+}
+/*void Data::Make_Forest( short *Dolans_t ){
+    this->k= new knot;
 	k->id= ci;
 	ci++;
 	k->knots+= Dolans_t[ cl ];
@@ -16,7 +35,7 @@ void Data::Make_Forest( short *Dolans_t, knot *k ){
 		}
 		cl++;
 		for( int i=0; i <Dolans_t[cl]; i++ ){
-			Make_Forest( Dolans_t, k->subgroups[ i ] );
+			Make_Forest_p( Dolans_t, k->subgroups[ i ] );
 		}
 	}else{
 		for( int i=0; i <Dolans_t[cl]; i++ ){
@@ -24,3 +43,23 @@ void Data::Make_Forest( short *Dolans_t, knot *k ){
 		}
 	}
 }
+
+void Data::Make_Forest_p( short *Dolans_t, knot *k ){
+    k= new knot;
+	k->id= ci;
+	ci++;
+	k->knots+= Dolans_t[ cl ];
+	if( Dolans_t[cl+1] != 0 ){//istnieja nizsze poziomy
+		for( int i=0; i <Dolans_t[cl]; i++ ){
+			k->subgroups[ i ]= new knot;
+		}
+		cl++;
+		for( int i=0; i <Dolans_t[cl]; i++ ){
+			Make_Forest_p( Dolans_t, k->subgroups[ i ] );
+		}
+	}else{
+		for( int i=0; i <Dolans_t[cl]; i++ ){
+			k->subgroups[ i ]= NULL;
+		}
+	}
+}*/
--- data.h
+++ data.h
@@ -1,36 +1,38 @@
 #ifndef dataH
 #define dataH
+
 /*
     Wszystkie informacje odnosnie calego planu lekcji(genotype).
     Zawieraj¹ 3 identyfikatory dla ka¿dego genu - nauczyciel, grupa, przedmiot.
     Na ka¿dym komputerze jest 1 obiekt tej klasy.
 */
+#include <vector>
+using namespace std;
 
-//id i poziomy liczone s¹ od 1
-//pozniej wejda jako static
-short cl= 1;//Current_level poziom drzewa na ktorym jestesmy
-short ci= 1;//current_id obecne id
+struct knot
+    {
+      static unsigned short knots;//liczba utworzonych wezlow
+      unsigned short id;
+      knot **subgroups;//podzial grupy na podgrupy
+      unsigned short *lessons;//lekcje jaka zawiera dana grupa
+      vector<unsigned short> checked;//sprawdzane wezla
+    };
 
 class Data
 {
 public:
     Data(unsigned char numberOfGroups);
-    ~Data();
-	void Make_Forest( short *Dolans_t, knot *k );//pozniej
+    //~Data();
+	void Make_Forest( short *Dolans_t );
+	//void Make_Forest( short *Dolans_t );//pozniej
 
 protected:
+	//void Make_Forest_p( short *Dolans_t, knot *k );
     unsigned char numerOfGroups;
     struct Lesson
     {
         unsigned char group, teacher, subject; //unikalne identyfikatory grupy, nauczyciela, przedmiotu
     } *tab; //wszystkie informacje o konkretnym genie
-    struct knot
-    {
-      static unsigned short knots;//liczba utworzonych wezlow
-      unsigned short id;
-      knot *subgroups;//podzial grupy na podgrupy
-      unsigned short *lessons;//lekcje jaka zawiera dana grupa
-      vector<unsigned short> checked;//sprawdzane wezla
-    } *k; //korzen lasu, najbardziej ogolna grupa
+    knot *k; //korzen lasu, najbardziej ogolna grupa
 };
 #endif
