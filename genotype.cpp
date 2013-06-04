b19f7e5819eedd4ae81612e2eb085cf7ce34f0dc
bartpr <bartpr@o2.pl>
2013-06-04 14:46:22 +0200

    okienka i kolizje nauczycieli bez wyciagniecia nauczycieli, dodanie stałych do eksperymentów

--- genotype.cpp
+++ genotype.cpp
@@ -10,6 +10,7 @@ Genotype::Genotype(const Data &data)
     numberOfGenes = data.numberOfLessons;
     numberOfRooms = data.numberOfRooms;
     numberOfTerms = data.numberOfTerms;
+    numberOfTerms = 23; //trzeba o skads wziac
     genes = new Gene*[numberOfGenes];
     srand(time(NULL));
     for(unsigned int i=0;i<numberOfGenes;i++)
@@ -42,21 +43,83 @@ void Genotype::Evaluation()
 
 double Genotype::termsCollision()
 {
-
+  double const collision = 1; //wspolczynnik kolizji terminosal
   double tmpPenalty= 0;
-  
+
   for( int i= 0; i< numberOfGenes; i++ )//sprawdzenia kazdej ts z kazda
     for( int j= i+1; j< numberOfGenes; j++ )
       if( genes[ i ]->term== genes[ j ]->term )
         if( genes[ i ]->room== genes[ j ]->room )
         {
           penalty= true;
-          tmpPenalty--;
+          tmpPenalty-= collision;
         }
 
   mark+= tmpPenalty;
   return tmpPenalty;
 
+}
+
+double Genotype::teachersEvaluation()
+{
+  //do poprawki
+  int nauczyciel = 22;//trzeba wyciagnac nauczyciela i zastpic kilka linijek nizej oznacznych ***
+  ///////////////////////////////////////////
+
+  double const window = 1; //wspolczynnik kary za okno
+  double const collision = 2; //wspolczynnik kary za kolizje
+
+  double tmpPenalty= 0;
+
+  bool **tab = new bool*[numberOfTeachers];
+  for (int i=0;i<numberOfTeachers;i++)
+    tab[i] = new bool[numberOfTerms];
+
+  for (int i=0;i<numberOfTeachers;i++)
+    for (int j=0;j<numberOfTerms;j++)
+      tab[i][j]=false;
+  //zerowanie tablicy, tam gdzie lekcja bedzie true
+
+  for( int i= 0; i< numberOfGenes; i++ )
+  {
+    if( tab[nauczyciel][genes[ i ]->term] ) //***
+    {
+      tmpPenalty -= 2; //kolizja nauczyciela
+      penalty= true;
+    }
+    else
+      tab[nauczyciel][genes[ i ]->term]=true; //***
+  }
+
+  int firstTerm, lastTerm, thisDayTerms;
+  //pierwszy i ostatni termin, rozwojowo preferencje co do godzin nauczycieli (rano/wieczor)
+  for (int i=0;i<numberOfTeachers;i++)
+    for (int j=0;j<5;i++)//dzien tygodnia
+    {
+      firstTerm = lastTerm = -1;
+      thisDayTerms = 0;
+      for (int k=0;j<numberOfTerms/5;k++)//termin w dzien tygodnia
+      {
+        if(tab[i][j*(numberOfTerms/5)+k])
+        {
+          lastTerm = k;
+          thisDayTerms++;
+          if(firstTerm == -1)
+            firstTerm = k;
+        }
+      } //znajdowanie dla danego dnia ilosci przedmiotow, pierwszej oraz ostatniej lekcji
+
+      if (thisDayTerms>1)//jesli nie ma nawet 2 lekcji nie moze byc okienek
+       tmpPenalty -= window * (lastTerm - thisDayTerms - firstTerm + 1); //okno nauczyciela
+    }
+
+  for (int i=0;i<numberOfTeachers;i++)
+    delete [] tab[i];
+  delete [] tab;
+  //zwolnienie pamieci
+
+  mark+= tmpPenalty;
+  return tmpPenalty;
 }
 
 bool Genotype::Mark(double &mark)
@@ -67,6 +130,7 @@ bool Genotype::Mark(double &mark)
 
 double Genotype::collisionsInClass(Data &d, Data::Node *p, Data::Node *q)
 {
+
 	double tmpPenalty = 0;
 	if(p== 0 && q == 0)
 	{
--- genotype.h
+++ genotype.h
@@ -2,6 +2,7 @@
 #define genotypeH
 #include "gene.h"
 #include "data.h"
+#include "information.h"
 /*
     Jest to jeden osobnik - caly plan lekcji.
     Zawiera ocenê, karê, ilosc genów(grup) oraz tablicê wskaŸników na wszystkie geny.
@@ -14,12 +15,14 @@ public:
     void Evaluation(); //Funkcja oceniaj¹ca
     bool Mark(double &mark);
     double termsCollision();//kolizja terminosal
+    double teachersEvaluation();//kolizje i okienka nauczycieli
 	double collisionsInClass(Data &d, Data::Node *p = 0, Data::Node *q = 0);
 protected:
     Gene **genes; //Tablica wszystkich przedmiotów(po kolei wed³ug grup[klas])
     unsigned short numberOfGenes;
     unsigned char numberOfTerms;
     unsigned char numberOfRooms;
+    unsigned char numberOfTeachers;
     double mark;
     bool penalty;
 };
